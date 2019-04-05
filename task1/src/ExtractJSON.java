d470ab87f1f997c30a2aa91ff71f65962dcd518b
Peace Han <peacehan@gmail.com>
2019-04-05 11:51:46 -0400

    work on extractJSON

---
+++ task1/src/ExtractJSON.java
@@ -0,0 +1,61 @@
+/**
+ * code to extract and preprocess the Yelp data
+ * @author Peace Han
+ * @author Krupa Patel
+ */
+
+import java.io.FileReader;
+import java.util.Iterator;
+import java.util.Map;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.*;
+
+public class ExtractJSON {
+    public static void main(String[] args) throws Exception {
+        // parsing file "JSONExample.json"
+        Object obj = new JSONParser().parse(new FileReader("data/business.json"));
+
+        // typecasting obj to JSONObject
+        JSONObject jo = (JSONObject) obj;
+
+        // getting firstName and lastName
+        String business_id = (String) jo.get("business_id");
+        String business_name = (String) jo.get("name");
+
+        System.out.println(business_id);
+        System.out.println(business_name);
+
+        /*
+        // getting age
+        long age = (long) jo.get("age");
+        System.out.println(age);
+
+        // getting address
+        Map address = ((Map)jo.get("address"));
+
+        // iterating address Map
+        Iterator<Map.Entry> itr1 = address.entrySet().iterator();
+        while (itr1.hasNext()) {
+            Map.Entry pair = itr1.next();
+            System.out.println(pair.getKey() + " : " + pair.getValue());
+        }
+
+        // getting phoneNumbers
+        JSONArray ja = (JSONArray) jo.get("phoneNumbers");
+
+        // iterating phoneNumbers
+        Iterator itr2 = ja.iterator();
+
+        while (itr2.hasNext())
+        {
+            itr1 = ((Map) itr2.next()).entrySet().iterator();
+            while (itr1.hasNext()) {
+                Map.Entry pair = itr1.next();
+                System.out.println(pair.getKey() + " : " + pair.getValue());
+            }
+        }        */
+    }
+}
+
