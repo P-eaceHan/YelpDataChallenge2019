cccb2d606111e35b87c0de76ad6792f5c0231160
Krupa <krupahp90@gmail.com>
2019-04-08 18:15:58 -0400

    Added ExtractJSONtoCSV.java

---
+++ task1/src/ExtratJSONtoCSV.java
@@ -0,0 +1,74 @@
+/**
+ * code to extract and preprocess the Yelp data
+ * @author Peace Han
+ * @author Krupa Patel
+ */
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Iterator;
+import java.util.Map;
+
+import org.json.simple.JSONObject;
+import org.json.simple.JSONArray;
+import org.json.simple.parser.*;
+
+public class ExtractJson {
+	
+    public static void main(String[] args)
+    {
+           JSONParser parser = new JSONParser();
+           File file = new File("/Volumes/Krupa/MISStudy/Spring 2019/Search/Final Project/yelp_dataset/review.json");
+           
+           File outputfile = new File("/Volumes/Krupa/MISStudy/Spring 2019/Search/Final Project/IR_FinalProject/output/review.csv");
+           
+           try {
+        	   
+        	   System.out.println("Reading JSON file");
+        	  
+        	   BufferedReader br = new BufferedReader(new FileReader(file));
+        	   PrintWriter pw = new PrintWriter(outputfile);
+
+        	   String line;
+        	   
+        	   while((line = br.readLine()) != null) 
+        	   {
+//        		    System.out.println(line);
+        		    JSONObject json = (JSONObject) parser.parse(line); 
+               	    StringBuilder sb = new StringBuilder();
+//               	    String[] entries = {"business_id", "name", "stars", "review_count","categories"};
+               	    
+	   				String business_id = (String) json.get("business_id");
+	   				business_id = business_id.replaceAll(",", " ");
+	   				sb.append(business_id);
+	   				sb.append(',');
+	   				
+	   				String name = (String) json.get("review_id");
+	   				name = name.replaceAll(",", " ");
+	   				sb.append(name);
+	   				sb.append(",");
+	   				
+	   				String text = (String) json.get("text");
+	   				if(text != null) {
+	   					text = text.replaceAll("\n", " ");
+	   					text = text.replaceAll(",", " ");
+	   					sb.append(text);
+	   				}
+	   				
+        		    System.out.println("JSON : " + sb.toString());
+	   				pw.write(sb.toString());
+	   				   				
+	   				pw.println();  	   				
+        		   }
+   			pw.close();
+   			br.close();
+           }
+           catch(ParseException | IOException e) {
+        	   e.printStackTrace();
+           }
+    }
+}
+
